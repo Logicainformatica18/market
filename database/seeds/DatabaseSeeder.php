9cf5eb9cfe28adbf45924ae759ee658aa7aa1e04
Anthony Cardenas <logicainformatica18@gmail.com>
2021-01-12 22:31:57 -0500

    modificando seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
+       //$this->call(UsersTableSeeder::class);
     $this->call(PermissionsSeeder::class);
      $this->call(CategorySeeder::class);
      $this->call(ColorSeeder::class);
